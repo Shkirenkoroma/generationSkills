a5a3e0a82a2e6fd19eb2ae88a99388b20be6b129
Shkirenkoroma <shkirenkoroma@mail.ru>
2023-04-23 15:30:00 +0300

    feature/TMS-1002: adde a new style with arrow

---
+++ src/App.styles.jsx
@@ -0,0 +1,218 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	width: 100%;
+	max-width: 1440px;
+	min-height: 900px;
+	background-color: #252424;
+	margin: 0 auto;
+`;
+
+export const ContainerContent = styled.div`
+	width: 100%;
+	max-width: 1240px;
+	min-height: 800px;
+	margin: 0 auto;
+	background-color: #6e3232;
+`;
+
+export const Top = styled.header`
+	position: relative;
+	background-size: 100px 100px;
+	max-width: 1440px;
+	width: 100%;
+	height: 250px;
+	margin: 0 auto;
+`;
+
+export const MiddlePart = styled.section`
+	display: flex;
+	align-items: center;
+	justify-content: space-around;
+	max-width: 1240px;
+	width: 100%;
+	min-height: 371px;
+	background-color: #922424;
+	margin: 0 auto;
+`;
+
+export const BurgerMenu = styled.div`
+	position: relative;
+	min-height: 25px;
+	left: 100px;
+	top: 50px;
+	&:hover {
+		cursor: pointer;
+	}
+`;
+
+export const Lines = styled.span`
+	display: inline-block;
+	max-width: 33px;
+	width: 100%;
+	min-height: 3px;
+	background-color: #ffffff;
+	::before {
+		content: '';
+		position: absolute;
+		top: 6px;
+		left: 0;
+		height: 3px;
+		max-width: 33px;
+		width: 100%;
+		background-color: #ffffff;
+	}
+	::after {
+		content: '';
+		position: absolute;
+		bottom: 0;
+		left: 0;
+		height: 3px;
+		max-width: 33px;
+		width: 100%;
+		background-color: #ffffff;
+	}
+`;
+
+export const ImageTopLine = styled.img`
+	position: absolute;
+`;
+
+export const ImageBottomLine = styled.img``;
+
+export const Forward = styled.button`
+	width: 300px;
+	height: 60px;
+	border: none;
+	background: linear-gradient(93.71deg, #7a0bc0 13.27%, #fa58b6 101.64%);
+	border-radius: 30px;
+	&:hover {
+		cursor: pointer;
+		background: linear-gradient(11.71deg, #7a0bc0 13.27%, #fa58b6 101.64%);
+		background-position: 0;
+		transition: all 700ms;
+	}
+`;
+
+export const TextForward = styled.span`
+	font-family: 'Montserrat';
+	font-style: normal;
+	font-weight: 700;
+	font-size: 20px;
+	line-height: 24px;
+	color: #ffffff;
+`;
+
+export const Title = styled.h1`
+	font-family: 'Montserrat';
+	font-style: normal;
+	font-weight: 400;
+	font-size: 48px;
+	line-height: 59px;
+	color: #ffe9f6;
+`;
+
+export const Describe = styled.p`
+	max-width: 550px;
+	width: 100%;
+	font-family: 'Montserrat';
+	font-style: normal;
+	font-weight: 400;
+	font-size: 16px;
+	line-height: 20px;
+	color: #ffe9f6;
+`;
+
+export const LevelOne = styled.button`
+	width: 136px;
+	height: 38px;
+	background: transparent;
+	border: 1px solid #ffffff;
+	border-radius: 20px;
+	margin-bottom: 50px;
+
+	&:hover {
+		background-color: #7a0bc0;
+		cursor: pointer;
+		transition: all 700ms;
+	}
+`;
+
+export const LevelSecond = styled.button`
+	width: 136px;
+	height: 38px;
+	background: transparent;
+	border: 1px solid #ffffff;
+	border-radius: 20px;
+	margin-bottom: 50px;
+
+	&:hover {
+		background-color: #0b8ac0;
+		cursor: pointer;
+		transition: all 700ms;
+	}
+`;
+
+export const LevelThird = styled.button`
+	width: 136px;
+	height: 38px;
+	background: transparent;
+	border: 1px solid #ffffff;
+	border-radius: 20px;
+
+	&:hover {
+		background-color: #e105e5;
+		cursor: pointer;
+		transition: all 700ms;
+	}
+`;
+
+export const LevelText = styled.span`
+	font-family: 'Montserrat';
+	font-style: normal;
+	font-weight: 600;
+	font-size: 18px;
+	line-height: 22px;
+	color: #ffffff;
+`;
+
+export const LevelGroup = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+`;
+
+export const GenerationGroup = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: space-around;
+	max-width: 564px;
+	width: 100%;
+	min-height: 371px;
+`;
+export const BowlGroup = styled.div`
+	position: relative;
+`;
+
+export const BackBowl = styled.img``;
+export const ShortLine = styled.img`
+	position: absolute;
+	top: 60px;
+	right: 57px;
+`;
+export const Center = styled.img`
+	position: absolute;
+	top: 210px;
+	right: 215px;
+	z-index: 1;
+`;
+export const Arrow = styled.img`
+	position: absolute;
+	top: 41px;
+	right: 48px;
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,68 @@
-import './App.css';
+import * as S from './App.styles.jsx';
+import topLine from './img/topborder.png';
+import bottomLine from './img/bottomborder.png';
+import cicle from './img/blackbowl.png';
+import shortline from './img/linepop.png';
+import center from './img/cycle.png';
+import arrow from './img/arrow.png';
 
 const App = () => {
-  
-  return (
-    <div className="App">
-     
-    </div>
-  );
-}
+	return (
+		<S.Container>
+			<S.ContainerContent>
+				<S.Top>
+					<S.ImageTopLine
+						src={topLine}
+						alt="variable line above main section"
+						width="1240px"
+						height="250px"
+					/>
+					<S.BurgerMenu>
+						<S.Lines />
+					</S.BurgerMenu>
+				</S.Top>
+				<S.MiddlePart>
+					<div>
+						<S.Title>
+							Hey, let`s choose <br /> a gift booster
+						</S.Title>
+						<S.Describe>
+							The booster can include both a small set of gifts and a large one,
+							depending on the selected level of the generator
+						</S.Describe>
+						<S.Forward>
+							<S.TextForward>GO</S.TextForward>
+						</S.Forward>
+					</div>
+					<S.GenerationGroup>
+						<S.BowlGroup>
+							<S.BackBowl src={cicle} alt="" />
+							<S.ShortLine src={shortline} alt="" />
+							<S.Center src={center} />
+							<S.Arrow src={arrow} />
+						</S.BowlGroup>
+						<S.LevelGroup>
+							<S.LevelOne>
+								<S.LevelText>Level 1</S.LevelText>
+							</S.LevelOne>
+							<S.LevelSecond>
+								<S.LevelText>Level 2</S.LevelText>
+							</S.LevelSecond>
+							<S.LevelThird>
+								<S.LevelText>Level 3</S.LevelText>
+							</S.LevelThird>
+						</S.LevelGroup>
+					</S.GenerationGroup>
+				</S.MiddlePart>
+				<S.ImageBottomLine
+					src={bottomLine}
+					alt="variable line under main section"
+					width="1240px"
+					height="250px"
+				/>
+			</S.ContainerContent>
+		</S.Container>
+	);
+};
 
 export default App;
